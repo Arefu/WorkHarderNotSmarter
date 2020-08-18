eb12ff51a2fe4d6cb2a86b0ba4f8fe5dd0ed7300
Johnathon <JohnathonKM@Gmail.com>
2020-08-18 13:55:24 +1200

    Virtual SmartZone
    I'm back in Education baybay, so that means more powershell, this one is to interact and login to a virtual smartzone. Not on-prem. It's tested using 5.1.0.0.496. So far I have figured out logging in, I am working on emulating some other basic API calls that I require in my job, any other features, open an issue.

---
+++ Ruckus/Virtual Smart Zone/Ruckus-SmartZone.ps1
@@ -0,0 +1,27 @@
+﻿function New-RuckusSession
+{
+    param(
+        [Parameter(Mandatory=$true)]
+        [Uri]$Uri,
+        [Parameter(Mandatory=$true)]
+        [System.Management.Automation.PSCredential]$Credential
+    )
+    
+    $Execution = ((Invoke-WebRequest -Uri $Uri -SessionVariable Session).InputFields | Where-Object 'name' -Like 'execution').Value
+    $Body = $("username="+$($Credential.UserName)+"&password="+$($Credential.GetNetworkCredential().Password+"&execution="+$Execution+"&_eventId=submit&geolocation="))
+    $UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
+    $Time = [LONG](New-TimeSpan -Start (Get-Date "01/01/1970") -End (Get-Date)).TotalMilliseconds
+
+    $Headers = @{'Content-Type' = "text/plain;charset=UTF-8"}
+
+    Invoke-WebRequest -Method Post -Uri $Uri -Body $Body -UserAgent $UserAgent -WebSession $Session
+
+    #Invoke-WebRequest -Headers $Headers -Method Post -Uri ("https://SNIP/wsg/api/public/v8_0/query/client?_dc="+$Time) -WebSession $Session -Body '{"filters":[{"type":"DOMAIN","value":"SNIP"}],"fullTextSearch":{"type":"AND","value":""},"attributes":["*"],"sortInfo":{"sortColumn":"clientMac","dir":"ASC"},"page":1,"limit":8}'
+
+     
+}
+
+function Get-ActiveClient
+{
+}
+New-RuckusSession "https://SNIP/cas/login?service=/wsg/login/cas" 
