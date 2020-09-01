c0fd9d4faa16e9e8f596c64ab3d986eaa24c8c42
Johnathon <JohnathonKM@Gmail.com>
2020-09-01 12:27:48 +1200

    Added client tracking mwahahaha
    So this only works in my env, because I have the AP's location in the description.... yeah weird I know but it works.

--- Ruckus/Virtual Smart Zone/Ruckus-SmartZone.ps1
+++ Ruckus/Virtual Smart Zone/Ruckus-SmartZone.ps1
@@ -1,4 +1,7 @@
-﻿function New-RuckusSession
+﻿$Session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+$UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
+
+function New-RuckusSession
 {
     param(
         [Parameter(Mandatory=$true)]
@@ -7,14 +10,12 @@
         [System.Management.Automation.PSCredential]$Credential
     )
     
-    $Execution = ((Invoke-WebRequest -Uri $Uri -SessionVariable Session).InputFields | Where-Object 'name' -Like 'execution').Value
+    $Execution = ((Invoke-WebRequest -Uri $Uri -WebSession $Session).InputFields | Where-Object 'name' -Like 'execution').Value
     $Body = $("username="+$($Credential.UserName)+"&password="+$($Credential.GetNetworkCredential().Password+"&execution="+$Execution+"&_eventId=submit&geolocation="))
-    $UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
     $Time = [LONG](New-TimeSpan -Start (Get-Date "01/01/1970") -End (Get-Date)).TotalMilliseconds
-
     $Headers = @{'Content-Type' = "text/plain;charset=UTF-8"}
 
-    Invoke-WebRequest -Method Post -Uri $Uri -Body $Body -UserAgent $UserAgent -WebSession $Session
+    Invoke-WebRequest -Method Post -Uri $Uri -Body $Body -UserAgent $UserAgent -WebSession $Session | Out-Null
 
     #Invoke-WebRequest -Headers $Headers -Method Post -Uri ("https://SNIP/wsg/api/public/v8_0/query/client?_dc="+$Time) -WebSession $Session -Body '{"filters":[{"type":"DOMAIN","value":"SNIP"}],"fullTextSearch":{"type":"AND","value":""},"attributes":["*"],"sortInfo":{"sortColumn":"clientMac","dir":"ASC"},"page":1,"limit":8}'
 
@@ -24,4 +25,26 @@
 function Get-ActiveClient
 {
 }
+
+function Get-GetAPClientConnectedTo
+{
+    param(
+        [Parameter(Mandatory=$true)]
+        [String]$Client,
+        [Parameter(Mandatory=$true)]
+        [String]$Uri
+    )
+    $Time = [LONG](New-TimeSpan -Start (Get-Date "01/01/1970") -End (Get-Date)).TotalMilliseconds
+    #DOMAIN = Unique, Find Out Where It Is, Then Use IT?
+    $Body = "{`"filters`":[{`"type`":`"DOMAIN`",`"value`":`"SNIP`"}],`"fullTextSearch`":{`"type`":`"AND`",`"value`":`""+$Client+"`"},`"attributes`":[`"*`"],`"sortInfo`":{`"sortColumn`":`"clientMac`",`"dir`":`"ASC`"},`"page`":1,`"limit`":8}"
+    $Headers = @{'Content-Type' = "text/plain;charset=UTF-8"; 'X-Requested-With' = "XMLHttpRequest"}
+    $Reply = Invoke-WebRequest -UserAgent $UserAgent -Method Post -WebSession $Session -Uri ("https://"+$Uri+":8443/wsg/api/public/v8_0/query/client?_dc="+$Time) -Body $Body -Headers $Headers
+    $Content = $Reply.Content | ConvertFrom-Json
+   
+    $Reply = Invoke-WebRequest -UserAgent $UserAgent -Method Get -WebSession $Session -Uri ("https://"+$Uri+":8443/wsg/api/scg/aps/"+$Content.'list'[0].'apMac'+"/config?_dc="+$Time)
+    $Content = $Reply.Content | ConvertFrom-Json
+    $Content.'data'.'description'
+}
+
 New-RuckusSession "https://SNIP/cas/login?service=/wsg/login/cas" 
+Get-GetAPClientConnectedTo -Uri "SNIP" -Client "F8:1F:32:15:32:26"
